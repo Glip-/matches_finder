1a03797cde7e2a638b9911f8358857f642a56479
Glip <glip.aka.den@gmail.com>
2018-07-31 01:05:01 -0400

    added test.py and text files

--- matchesFinder.py
+++ matchesFinder.py
@@ -5,9 +5,10 @@ import time
 ### 1) standard input if no files are specified, or the file name '-' is given
 
 color = '\033[{0}m'
-colourStr = color.format(32)
+colorGreenStr = color.format(32)
 colorRedStr = color.format(31)
 resetStr = color.format(0)
+
 
 class outputPrinter:
     def __init__(self, match):
@@ -21,69 +22,77 @@ class outputPrinter:
     def colorPrint(self, filename, output):
         print("\n\nThere are matches in file '" + str(filename) + "':\n")
         for t in output:
-            coloredText = ''
+            self.coloredText = ''
             lastMatch = 0
 
             for m in self.match.finditer(t):
                 start, end = m.span()
-                coloredText += t[lastMatch: start]
-                coloredText += colourStr
-                coloredText += t[start: end]
-                coloredText += resetStr
+                self.coloredText += t[lastMatch: start]
+                self.coloredText += colorGreenStr
+                self.coloredText += t[start: end]
+                self.coloredText += resetStr
                 lastMatch = end
-            coloredText += t[lastMatch:]
-            print(coloredText)
+            self.coloredText += t[lastMatch:]
+            print(self.coloredText)
 
     def underscorePrint(self, filename, output):
         print("\n\nThere are matches in file '" + str(filename) + "':\n")
         for t in output:
-            underscoreText = ''
+            self.underscoreText = ''
             lastMatch = 0
 
             for m in self.match.finditer(t):
                 start, end = m.span()
-                underscoreText += " " * (start - lastMatch)
-                underscoreText += "^" * (end - start)
+                self.underscoreText += " " * (start - lastMatch)
+                self.underscoreText += "^" * (end - start)
                 lastMatch = end
-            underscoreText += " " * (lastMatch)
+                self.underscoreText += " " * (lastMatch)
             print(t)
-            print(underscoreText)
+            print(self.underscoreText)
 
     def machinePrint(self, filename, output):
         for t in output:
             lineNumber = t.split()[0]
-            outputText = ''
+            self.outputText = ''
             foundMatches = 0
             for m in self.match.finditer(t):
                 if foundMatches == 1:
-                    outputText += "\n"
+                    self.outputText += "\n"
                 start, end = m.span()
-                outputText += ":".join([filename, str(lineNumber), str(start), str(t[start: end])])
+                self.outputText += ":".join([filename, str(lineNumber), str(start), str(t[start: end])])
                 foundMatches = 1
-            print(outputText)
+            print(self.outputText)
 
 
 class matchesFinder:
-    def __init__(self, listOfFiles, match, printStyle):
-        self.listOfFiles = listOfFiles
-        self.match = match
-        self.op = outputPrinter(match)
-        self.printStyle = printStyle
+    def __init__(self, args):
+
+        self.listOfFiles = args.filenames.split(",")
+        self.match = re.compile(args.regexp)
+        self.op = outputPrinter(self.match)
+        self.printStyle = 0
+        if args.color:
+            self.printStyle = 1
+        elif args.underscore:
+            self.printStyle = 2
+        elif args.machine:
+            self.printStyle = 3
+
 
         for filename in self.listOfFiles:
             self.output = self.find(filename)
 
             if self.output:
-                if printStyle == 1:
+                if self.printStyle == 1:
                     self.op.colorPrint(filename, self.output)
-                elif printStyle == 2:
+                elif self.printStyle == 2:
                     self.op.underscorePrint(filename, self.output)
-                elif printStyle == 3:
+                elif self.printStyle == 3:
                     self.op.machinePrint(filename, self.output)
                 else:
                     self.op.normalPrint(filename, self.output)
             elif self.output == None:
-                print("\n\nCan't open file " + colorRedStr + str(filename) + resetStr)
+                print("\n\nCan not open file " + colorRedStr + str(filename) + resetStr)
             else:
                 print("\n\nNo Matches in file " + str(filename))
 
@@ -102,8 +111,6 @@ class matchesFinder:
             return None
 
 
-
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Matches Finder')
     parser.add_argument("-r", "--regexp",
@@ -114,29 +121,20 @@ def parse_arguments():
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-c", "--color",
-                          help="Optional, Highlight matching text with " + colourStr + "color" + resetStr, action='store_true')
+                          help="Optional, Highlight matching text with " + colorGreenStr + "color" + resetStr, action='store_true')
     group.add_argument("-u", "--underscore",
                           help="Optional, Print '^' under the matching text", action='store_true')
     group.add_argument("-m", "--machine",
                           help="Optional, Generate machine readable output", action='store_true')
 
-    return parser.parse_args()
+    return parser
 
 
 def main():
-    args = parse_arguments()
+    parser = parse_arguments()
+    args = parser.parse_args()
 
-    filenames = args.filenames.split(",")
-    match = re.compile(args.regexp)
-    printStyle = 0
-    if args.color:
-        printStyle = 1
-    elif args.underscore:
-        printStyle = 2
-    elif args.machine:
-        printStyle = 3
-
-    matchesFinder(filenames, match, printStyle)
+    matchesFinder(args)
 
 
 if __name__ == "__main__":
---
+++ tests.py
@@ -0,0 +1,78 @@
+import matchesFinder
+import unittest
+import time
+
+
+class TestMatchesFinder(unittest.TestCase):
+    def test_without_required_args(self):
+        """
+        User didn't pass all required args
+        """
+        self.parser = matchesFinder.parse_arguments()
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args([])
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['-r', 'rrr'])
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['-f', 'fff'])
+
+    def test_cannot_open_file(self):
+        """
+        User passed wrong name of text file
+        """
+        self.parser = matchesFinder.parse_arguments()
+        self.args = self.parser.parse_args(['-r', 'abcd', '-f', 'wrong'])
+        self.m = matchesFinder.matchesFinder(self.args)
+        self.assertEqual(self.m.output, None)
+
+    def test_no_matches_in_a_file(self):
+        """
+        Can't find any matches in the file
+        """
+        self.parser = matchesFinder.parse_arguments()
+        self.args = self.parser.parse_args(['-r', 'abcd', '-f', 'file1'])
+        self.m = matchesFinder.matchesFinder(self.args)
+        self.assertEqual(self.m.output, [])
+
+    def test_found_matches_normal_output__one_file(self):
+        """
+        Can't find any matches in the file
+        """
+        self.parser = matchesFinder.parse_arguments()
+        self.args = self.parser.parse_args(['-r', '[S,s]witch', '-f', 'file1'])
+        self.m = matchesFinder.matchesFinder(self.args)
+        self.assertEqual(len(self.m.output), 3)
+
+    def test_found_matches_color_output__two_files(self):
+        """
+        Found matches and colored text
+        """
+        self.parser = matchesFinder.parse_arguments()
+        self.args = self.parser.parse_args(['-r', '[S,s]witch', '-f', 'file1,file2', '-c'])
+        self.m = matchesFinder.matchesFinder(self.args)
+        self.assertEqual(len(self.m.output), 4)
+        self.assertIn("32m", self.m.op.coloredText)
+
+    def test_found_matches_underscore_output__two_files(self):
+        """
+        Found matches in two files and underscored it
+        """
+        self.parser = matchesFinder.parse_arguments()
+        self.args = self.parser.parse_args(['-r', '[S,s]witch', '-f', 'file1,file2', '-u'])
+        self.m = matchesFinder.matchesFinder(self.args)
+        self.assertEqual(len(self.m.output), 4)
+        self.assertIn("^", self.m.op.underscoreText)
+
+    def test_found_matches_machine_readable_output__one_file(self):
+        """
+        Found matches and create machine readable output
+        """
+        self.parser = matchesFinder.parse_arguments()
+        self.args = self.parser.parse_args(['-r', '[S,s]witch', '-f', 'file2', '-m'])
+        self.m = matchesFinder.matchesFinder(self.args)
+        self.assertEqual(len(self.m.output), 4)
+        self.assertEqual(len(self.m.op.outputText.split(":")), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
